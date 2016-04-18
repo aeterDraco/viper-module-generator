8829b6ab51552b7177b9f7a1ce64ee99a850d505
Ricardo Quintero <ricardo.quintero.botia@schibsted.hu>
2016-04-18 11:42:59 +0200

    Default template set to jofogas + path is optional, by default the current path is taken

--- lib/vipergen/viperthor.rb
+++ lib/vipergen/viperthor.rb
@@ -3,10 +3,11 @@ require 'vipergen'
 
 module Vipergen
   class ViperThor < Thor
+    defaultPath = Dir.pwd
 	desc "generate", "Generate a VIPER module"
 	option :language, :required => false, :default => 'objc', :type => :string, :desc => "The language of the generated module (swift, objc)"
-	option :template, :required => false, :default => 'default', :type => :string , :desc => "Template for the generation"
-	option :path, :required => true, :type => :string , :desc => "Path where the output module is going to be saved"
+	option :template, :required => false, :default => 'jofogas', :type => :string , :desc => "Template for the generation"
+	option :path, :required => false, :default => defaultPath, :type => :string , :desc => "Path where the output module is going to be saved"
 	def generate(name)
 	   Vipergen::Generator.generate_viper(options[:template], options[:language], name, options[:path])
 	end
